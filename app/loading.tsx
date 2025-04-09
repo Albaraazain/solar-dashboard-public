d8209bf4b0c26667fbe903eb88461991ebdaceac
albaraazain <albaraazain33@gmail.com>
2025-04-09 19:53:25 +0300

    refactor: remove Loading and LoadingScreen components

--- app/loading.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import LoadingScreen from '../components/ui/LoadingScreen';
-
-const Loading: React.FC = () => {
-  return <LoadingScreen />;
-};
-
-export default Loading;
